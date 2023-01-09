6125437add9df8acb4f57bd969ab94d74c3a87fb
Khaibar Saadat <khybersaadat93@gmail.com>
2023-01-09 22:06:37 +0100

    Complete layout component that has navbar and will have footer later

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import Navbar from "../src/components/header/Navbar";
+import Layout from "../src/components/layout/Layout";
 import Home from "../src/components/home/Home";
 import HorizLine from "./components/ui/HorizLine";
 import AboutMe from "../src/components/about-me/AboutMe";
@@ -8,17 +8,14 @@ import MyWork from "../src/components/my-work/MyWork";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Home />
-        <HorizLine />
-        <AboutMe />
-        <HorizLine />
-        <MyWork />
-        <HorizLine />
-      </main>
-    </div>
+    <Layout className="App">
+      <Home />
+      <HorizLine />
+      <AboutMe />
+      <HorizLine />
+      <MyWork />
+      <HorizLine />
+    </Layout>
   );
 }
 

e93b3bb407b786e5ac36000d89fdd22d1376d3d2
Khaibar Saadat <khybersaadat93@gmail.com>
2023-01-10 18:48:38 +0100

    Initialize mobile version of contact-me section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "../src/components/home/Home";
 import HorizLine from "./components/ui/HorizLine";
 import AboutMe from "../src/components/about-me/AboutMe";
 import MyWork from "../src/components/my-work/MyWork";
+import ContactMe from "../src/components/contact-me/ContactMe";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <AboutMe />
       <HorizLine />
       <MyWork />
+      <HorizLine />
+      <ContactMe />
       <HorizLine />
     </Layout>
   );

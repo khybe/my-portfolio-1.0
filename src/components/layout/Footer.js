44cbd497fd6a9e58913efb621834e80254509794
Khaibar Saadat <khybersaadat93@gmail.com>
2023-01-23 21:11:37 +0100

    Reduce footer content to only address related content + some media query styles ofTablet and Desktop screens

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,39 +6,6 @@ import { AiOutlineCopyright } from "react-icons/ai";
 const Footer = () => {
   return (
     <section id="footer">
-      <nav className="footer-nav">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about-me">About Me</a>
-          </li>
-          <li>
-            <a href="#my-work">My Work</a>
-          </li>
-          <li>
-            <a href="#contacts">Contacts</a>
-          </li>
-          <li>
-            <a href="#contact-me">Contact Me</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="footer-links">
-        <a
-          href="https://www.linkedin.com/in/khaibar-saadat-6a3858208/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin />
-          <span>Linkedin</span>
-        </a>
-        <a href="https://github.com/khybe" target="_blank" rel="noreferrer">
-          <FaGithub />
-          <span>github</span>
-        </a>
-      </div>
       <address className="created-by">
         <p>
           Written by:
@@ -51,9 +18,6 @@ const Footer = () => {
           <span>
             <a href="sabina.bastiova@gmail.com">Sabina Bastiova</a>
           </span>
-        </p>
-        <p>
-          Location: <span> Czech Republic</span>
         </p>
         <p>
           <AiOutlineCopyright />

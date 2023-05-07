ccfb094990f77204b8f0658bea4a73ddb6e573a1
Khaibar Saadat <khybersaadat93@gmail.com>
2023-05-07 23:26:53 +0200

    Creating slide in and slide out Animation for DetailModal

--- src/components/my-work/components/Detail.js
+++ src/components/my-work/components/Detail.js
@@ -1,17 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 import Backdrop from "../../ui/Backdrop";
 import "./Detail.css";
 
 const DetailModal = (props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowModal(true);
+    }, 10);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const closeModalHandler = () => {
+    setShowModal(false);
+    setTimeout(() => {
+      props.onCancel();
+    }, 400);
+  };
+
+  const modalClasses = `modal ${showModal ? "open" : "closed"}`;
+
   return ReactDOM.createPortal(
     <Fragment>
-      <Backdrop onClick={props.onCancel} />
-      <div className="modal">
+      <Backdrop onClick={closeModalHandler} />
+      <div className={modalClasses}>
         <div className="detail-content">
           <AiOutlineClose
-            onClick={props.onCancel}
+            onClick={closeModalHandler}
             className="detail-content__close-icon"
             size="2.5rem"
           />
@@ -50,55 +68,3 @@ const DetailModal = (props) => {
 };
 
 export default DetailModal;
-
-// import React, { Fragment } from "react";
-// import { AiOutlineClose } from "react-icons/ai";
-
-// import Backdrop from "../../ui/Backdrop";
-// import "./Detail.css";
-
-// const DetailModal = (props) => {
-//   return (
-//     <Fragment>
-//       <Backdrop onClick={props.onCancel} />
-//       <div className="modal">
-//         <div className="detail-content">
-//           <AiOutlineClose
-//             onClick={props.onCancel}
-//             className="detail-content__close-icon"
-//             size="2.5rem"
-//           />
-//           <h2>{props.title}</h2>
-//           <hr />
-//           <p>{props.description}</p>
-//           <hr />
-
-//           <footer className="detail-content__actions">
-//             {props.webLink && (
-//               <a
-//                 href={props.webLink}
-//                 target="_blank"
-//                 rel="noreferrer"
-//                 className="button"
-//               >
-//                 Website
-//               </a>
-//             )}
-//             {props.githubLink && (
-//               <a
-//                 href={props.githubLink}
-//                 target="_blank"
-//                 rel="noreferrer"
-//                 className="button"
-//               >
-//                 Github
-//               </a>
-//             )}
-//           </footer>
-//         </div>
-//       </div>
-//     </Fragment>
-//   );
-// };
-
-// export default DetailModal;
